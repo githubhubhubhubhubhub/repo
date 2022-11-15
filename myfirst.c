706dfa082331423d6731b3faaa00fded1c3fe404
githubhubhubhubhubhub <118278947+githubhubhubhubhubhub@users.noreply.github.com>
2022-11-15 21:20:16 +0530

    Add files via upload

---
+++ myfirst.c
@@ -0,0 +1,14 @@
+//imprecise age calculator
+#include <stdio.h>
+void main()
+{
+int by, cy;
+printf("Welcome to the imprecise age calculator! \nPlease enter your birth year.\n");
+scanf("%d", &by);
+printf("Enter current year\n");
+scanf("%d", &cy);
+int oa, ya;
+oa=cy-by;
+ya=oa-1;
+printf("Your age is either %d years or %d years", ya , oa);
+}
